dfc70efb37c3aea076e20d0ac0f89fa67fcbbb3e
Jimmy Smutek <jimmy@smutek.net>
2017-09-13 23:55:00 -0400

    Start cleanup, split script into functions

--- scraper.py
+++ scraper.py
@@ -2,41 +2,63 @@ from bs4 import BeautifulSoup
 import requests
 import html5lib
 
-# base url to start
-base_url = "https://www.google.com/search?as_q=&as_epq=%22Dean+Klag%22+site" \
-           ":jhsph.edu&start="
-# increment in 10's  (stop at 240)
-start = 0
-# url to target
-target_url = base_url + str(start)
-# get the page
-page = requests.get(target_url)
-# get html from the page
-soup = BeautifulSoup(page.text, "html5lib")
-# search results are in a div with ID of ires
-container = soup.find(id="ires")
-# and wrapped in an ordered list
-list = container.find("ol")
-# empty container for entries
-entries = []
-# process the list
-for child in list.children:
+
+def clean_link(link):
+    clean = link.strip("/url?q=").split("&", 1)[0]
+    return clean
+
+
+def create_entry(html):
     # grab the title
-    title = child.h3.text
+    title = html.h3.text
     # grab the link
-    href = child.a.get("href")
+    href = html.a.get("href")
+    link = clean_link(href)
     # returns with query strings on both ends, remove these
-    link = href.strip("/url?q=").split("&", 1)[0]
     # grab the description
-    description = child.find('span', attrs={'class': 'st'}).text
+    description = html.find('span', attrs={'class': 'st'}).text
     # stash the relevant bits in a dict
-    entry = {
+    data = {
         "Title": title,
         "Link": link,
         "Description": description,
         "Action Required": ""
     }
-    # add to entries list
-    entries.append(entry)
+    return data
 
-print(entries)
+
+def process_page(start):
+    # base url to start
+    base_url = "https://www.google.com/search?as_q=&as_epq=%22Dean+Klag%22+site" \
+           ":jhsph.edu&start="
+    # url to target
+    target_url = base_url + str(start)
+    # get the page
+    page = requests.get(target_url)
+    # get html from the page
+    soup = BeautifulSoup(page.text, "html5lib")
+    # search results are in a div with ID of ires
+    container = soup.find(id="ires")
+    # and wrapped in an ordered list
+    ol = container.find("ol")
+    return ol
+
+
+# application
+if __name__ == "__main__":
+
+    # increment in 10's  (stop at 240)
+    start = 0
+    increment = 10
+    end = 240
+
+    ol = process_page(start)
+    # empty container for entries
+    entries = []
+    # process the list
+    for child in ol.children:
+        entry = create_entry(child)
+    # add to entries list
+        entries.append(entry)
+
+    print(entries)
